9d8fa94c34ab05e4d99c166fabbb5998abfd86b2
Michael Naumenko <agaspher@outlook.com>
2014-09-20 00:15:54 +0400

    Cache sample

---
+++ CacheSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CacheSample.MvcApplication" Language="C#" %>
---
+++ CacheSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CacheSample.Startup))]
+namespace CacheSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
